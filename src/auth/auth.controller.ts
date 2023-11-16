4da09f76c5c694d3f1210e85cd5c7c79ebaffb70
luminous <910667665@qq.com>
2023-11-16 12:07:51 +0800

    feat：引入class-transformer&class-validator，进行管道校验

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { TypeormFilter } from 'src/filters/typeorm.filter';
+import { SigninUserDto } from './dto/sign-in.dto';
 
 @Controller('auth')
 @UseFilters(new TypeormFilter())
@@ -14,24 +15,24 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signin')
-  signIn(@Body() dto: any) {
+  signIn(@Body() dto: SigninUserDto) {
     const { username, password } = dto;
     return this.authService.signin(username, password);
   }
 
   @Post('/signup')
-  signUp(@Body() dto: any) {
+  signUp(@Body() dto: SigninUserDto) {
     const { username, password } = dto;
 
-    // 用户名/密码不得为空
-    if (!username || !password) {
-      throw new HttpException('用户名或密码不得为空', 400);
-    }
+    // // 用户名/密码不得为空
+    // if (!username || !password) {
+    //   throw new HttpException('用户名或密码不得为空', 400);
+    // }
 
-    // 其余校验规则，使用正则，比如长度、不符合格式等
-    if (username.length < 6 || password.length < 6) {
-      throw new HttpException('用户名或密码长度不得少于六位', 400);
-    }
+    // // 其余校验规则，使用正则，比如长度、不符合格式等
+    // if (username.length < 6 || password.length < 6) {
+    //   throw new HttpException('用户名或密码长度不得少于六位', 400);
+    // }
 
     return this.authService.signup(username, password);
   }
